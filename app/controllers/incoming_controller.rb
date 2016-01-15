bbfcc7041431c944a35a902832b44f349875234f
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2016-01-15 08:31:52 -0500

    Update

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,6 +1,6 @@
 class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
-  skip_before_action :authenticate_user
+  skip_before_action :authenticate_user!
 
   def create
 
