2b2eb604829694330933b9bd928730106ee7c4ce
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2016-01-15 08:54:06 -0500

    Stripped Text

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -7,7 +7,7 @@ class IncomingController < ApplicationController
     puts "INCOMING PARAMS HERE: #{params}"
     @user = User.find_by(email: params["sender"])
     @topic = Topic.find_by(title: params["subject"])
-    @url = params[:"body-plain"]
+    @url = params[:"stripped-text"]
     puts @url
 
     if @user.nil?
