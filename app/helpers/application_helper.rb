3311ee868c97591b6604b47fdb744e167b02132d
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2016-01-18 14:56:56 -0500

    URL Preview Thumbnail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  require 'embedly'
+
+  def embedly_preview(bookmark)
+    embedly_api =
+    Embedly::API.new(key: ENV['EMBEDLY_API_KEY'])
+    obj = embedly_api.oembed(url: bookmark.url, thumbnail_width: 250, thumbnail_height: 250)
+    image_tag(obj.first.thumbnail_url)
+
+    # p obj
+    # (obj.first.html).html_safe
+  end
 end
